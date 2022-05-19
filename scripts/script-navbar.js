70e8b297d6d5561b3f480ba76d9e1030bc16aa6a
Michelle Tran <v-trmic@microsoft.com>
2022-05-19 12:52:15 -0400

    created new scripts folder and moved js code out of header.html and into its own js file and updated header.html to pull in the script

---
+++ scripts/script-navbar.js
@@ -0,0 +1,15 @@
+var linkBlock = document.getElementsByClassName("link-block");
+var i;
+
+const url = window.location.href;
+const segments = url.split("/");
+const lastSegment = segments[segments.length - 2];
+
+var pageLink = document.getElementById(lastSegment);
+
+if(pageLink) {
+  pageLink.classList.toggle("active");
+} else {
+  homeLink = document.getElementById("home");
+  homeLink.classList.toggle("active");
+}
