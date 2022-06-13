f0b9b4cbe35af37b35f5d01f4c10a344e5154417
bmirza-affirma <bmirza@affirma.com>
2022-06-13 16:18:54 +0300

    Added responsive header

--- scripts/script-navbar.js
+++ scripts/script-navbar.js
@@ -6,10 +6,16 @@ const segments = url.split("/");
 const lastSegment = segments[segments.length - 2];
 
 var pageLink = document.getElementById(lastSegment);
+var toggleMenuButton = document.getElementById('toggle-menu');
+var menuContent = document.getElementById('links-menu');
 
 if(pageLink) {
-  pageLink.classList.toggle("active");
+  pageLink.classList.toggle('active');
 } else {
-  homeLink = document.getElementById("home");
-  homeLink.classList.toggle("active");
+  homeLink = document.getElementById('home');
+  homeLink.classList.toggle('active');
 }
+
+toggleMenuButton.addEventListener('click', () => {
+  menuContent.classList.toggle('active');
+})
